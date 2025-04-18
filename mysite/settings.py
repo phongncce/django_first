64aa67d3d865673b03a82b4534d3eb5374403c0b
Phong Nguyen <caophongncpj@gmail.com>
2025-04-18 23:21:29 +0700

    [DF-002] Add models for the app
    - Configure database
    - Adjust configs for deployment with Docker

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls.apps.PollsConfig',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +75,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,21 @@
+from datetime import datetime
+
 from django.db import models
 
+
 # Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    create_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"[{self.create_date.strftime("%Y-%M-%d")}] {self.question_text}"
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"[{self.question.id}] {self.choice_text}"
