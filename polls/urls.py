f556cf4c7f8b762a2fd2a8c3b6563e599755b126
Phong Nguyen <caophongncpj@gmail.com>
2025-05-26 21:19:53 +0700

    [DF-004] Use generic views for index, detail and result

--- polls/urls.py
+++ polls/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 app_name = "polls"
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("details/<int:question_id>/", views.detail, name="detail"),
-    path("results/<int:question_id>/", views.results, name="result"),
+    path("", views.IndexView.as_view(), name="index"),
+    path("details/<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("results/<int:pk>/", views.ResultsView.as_view(), name="result"),
     path("vote/<int:question_id>", views.vote, name="vote"),
 ]
