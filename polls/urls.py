5edc86bc01e3c1d5b321bfecbd76c17257dcbdf4
Phong Nguyen <caophongncpj@gmail.com>
2025-05-26 20:50:13 +0700

    [DF-003] Add endpoints and views for question list & details

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = "polls"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("details/<int:question_id>/", views.detail, name="detail"),
+    path("results/<int:question_id>/", views.results, name="result"),
+    path("vote/<int:question_id>", views.vote, name="vote"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,23 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from polls.models import Question
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-create_date')[:5]
+    return render(request, "polls/index.html", {"latest_question_list": latest_question_list})
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking at the results of question {question_id}.")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
